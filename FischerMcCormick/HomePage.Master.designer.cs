afd51dd3eb87d922aefb48a744aeebfefff40f97
Joshua Fischer <33161382+joshuadfischer817@users.noreply.github.com>
2017-11-30 13:02:35 -0600

    banner

--- FischerMcCormick/HomePage.Master.designer.cs
+++ FischerMcCormick/HomePage.Master.designer.cs
@@ -40,6 +40,15 @@ namespace FischerMcCormick {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder consistentBanner;
         
         /// <summary>
+        /// TheRightPlaceBanner control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label TheRightPlaceBanner;
+        
+        /// <summary>
         /// NavigationBarPlaceHolder control.
         /// </summary>
         /// <remarks>

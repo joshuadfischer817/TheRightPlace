23d8a46b13253e6a4f8496952fc2b02c511e5307
Joshua Fischer <33161382+joshuadfischer817@users.noreply.github.com>
2017-11-30 13:48:54 -0600

    Events

--- FischerMcCormick/Events.aspx
+++ FischerMcCormick/Events.aspx
@@ -1,5 +1,16 @@
 ﻿<%@ Page Language="C#" MasterPageFile="~/HomePage.Master" AutoEventWireup="true" CodeBehind="Events.aspx.cs" Inherits="FischerMcCormick.Events" %>
 
 <asp:Content ID="mainEvents" ContentPlaceHolderID="mainContentPlaceHolder" runat="server">
-    Information about the upcoming events.
+    <asp:Label ID="EventsLabel" runat="server" Font-Bold="True" Font-Size="Larger" Font-Underline="True" Text="Upcoming Events" Width="203px"></asp:Label>
+    <br />
+    <asp:Label ID="Label1" runat="server" Font-Bold="True" Font-Size="Large" Text="Kate and Jeffs Wedding"></asp:Label>
+&nbsp;&nbsp;
+    <br />
+    <asp:Label ID="Label2" runat="server" Text="The wedding is on 12/12/2017"></asp:Label>
+    <br />
+    <br />
+    <asp:Label ID="Label3" runat="server" Font-Bold="True" Font-Size="Large" Text="Little People, Big Ideas Art Show"></asp:Label>
+    <br />
+    <asp:Label ID="Label4" runat="server" Text="The Art Show will be Friday 12/7/2017 - Sunday 12/9/2017"></asp:Label>
+&nbsp;
 </asp:Content>
--- FischerMcCormick/Events.aspx.designer.cs
+++ FischerMcCormick/Events.aspx.designer.cs
@@ -11,5 +11,50 @@ namespace FischerMcCormick {
     
     
     public partial class Events {
+        
+        /// <summary>
+        /// EventsLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label EventsLabel;
+        
+        /// <summary>
+        /// Label1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+        
+        /// <summary>
+        /// Label2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+        
+        /// <summary>
+        /// Label3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
+        
+        /// <summary>
+        /// Label4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label4;
     }
 }
